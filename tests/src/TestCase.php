7f05a5900aa45ec80996e059082cf0c5c4b32295
mckenziearts <mckenziearts@users.noreply.github.com>
2023-10-03 07:08:56 +0000

    Fix code styling

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -23,7 +23,7 @@ abstract class TestCase extends BaseTestCase
     protected function defineDatabaseMigrations(): void
     {
         $this->loadLaravelMigrations();
-        $this->loadMigrationsFrom(__DIR__. '/../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 
     protected function getEnvironmentSetUp($app): void
