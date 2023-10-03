d6559d64306030268937dcba2d74ad1527123cb3
Arthur Monney <monneylobe@gmail.com>
2023-10-03 08:15:45 +0200

    Add install command

--- src/SubscriptionServiceProvider.php
+++ src/SubscriptionServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Laravelcm\Subscriptions;
 
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -12,11 +13,12 @@ final class SubscriptionServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name('laravel-subscriptions')
-            ->hasConfigFile('laravel-subscriptions');
-    }
-
-    public function bootingPackage(): void
-    {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+            ->hasConfigFile('laravel-subscriptions')
+            ->hasInstallCommand(function (InstallCommand $command): void {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToStarRepoOnGitHub('laravelcm/laravel-subscriptions');
+            });
     }
 }
